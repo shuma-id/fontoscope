8fca26c8d6ca93400e171ead0e47906d0a97717d
bardoor <dmitrybartenev58@gmail.com>
2025-06-20 16:48:10 +0300

    feat: add Adapter behaviour

---
+++ lib/adapter.ex
@@ -0,0 +1,20 @@
+defmodule FontoScope.Adapter do
+  @moduledoc false
+
+  @type file_path :: String.t() | Path.t()
+
+  @callback extract(file_path()) :: {:ok, FontInfo.t()} | {:error, String.t()}
+
+  defmacro __using__(opts) do
+    extensions = Keyword.fetch!(opts, :extensions)
+
+    quote do
+      @behaviour FontoScope.Adapter
+
+      @supported_extensions unquote(extensions)
+
+      def supported_extensions, do: @supported_extensions
+    end
+  end
+
+end
