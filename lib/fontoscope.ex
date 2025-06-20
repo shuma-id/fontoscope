358f556035ea3aee0a3bcb2b5a8b7a156d51b2d4
bardoor <dmitrybartenev58@gmail.com>
2025-06-20 16:50:24 +0300

    feat: add FontoScope module

---
+++ lib/fontoscope.ex
@@ -0,0 +1,26 @@
+defmodule FontoScope do
+  @moduledoc false
+
+  alias FontoScope.AdapterRegistry
+
+  @doc """
+  Extract font info from given file
+  """
+  @spec extract(String.t()) :: {:ok, FontInfo.t()} | {:error, String.t()}
+  def extract(file_path) do
+    extension = get_extension(file_path)
+
+    with {:ok, adapter} <- AdapterRegistry.find_by_extension(extension),
+         {:ok, font_info} <- adapter.extract(file_path) do
+      {:ok, font_info}
+    end
+  end
+
+  defp get_extension(file_path) do
+    file_path
+    |> Path.extname()
+    |> String.downcase()
+    |> String.trim_leading(".")
+  end
+
+end
