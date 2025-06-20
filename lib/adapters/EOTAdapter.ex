23bb9fe6b2a93a8669ef703446b40bb5f600c827
bardoor <dmitrybartenev58@gmail.com>
2025-06-20 16:49:11 +0300

    feat: add adapter for .eot

---
+++ lib/adapters/EOTAdapter.ex
@@ -0,0 +1,41 @@
+defmodule FontoScope.EOTAdapter do
+  @moduledoc """
+  Adapter for EOT files
+  """
+  use FontoScope.Adapter, extensions: ~w(eot)
+
+  @doc """
+  Extract font info from given file
+  """
+  @spec extract(String.t()) :: {:ok, FontInfo.t()} | {:error, String.t()}
+  def extract(path) do
+    parse_eot_file(path)
+  end
+
+  defp parse_eot_file(eot_path) do
+    with_temp_file("", "ttf", fn ttf_path ->
+      args = ~w(#{eot_path} #{ttf_path})
+
+      case System.cmd("eot2ttf", args, stderr_to_stdout: true) do
+        {_, 0} -> TTXAdapter.extract(ttf_path)
+        {error, _} -> {:error, error}
+      end
+    end)
+  end
+
+  defp with_temp_file(content, extension, function) do
+    temp_path = Path.join(
+      System.tmp_dir(),
+      "font_meta_#{:erlang.unique_integer([:positive])}.#{extension}"
+    )
+
+    File.write!(temp_path, content)
+
+    result = function.(temp_path)
+
+    File.rm!(temp_path)
+
+    result
+  end
+
+end
