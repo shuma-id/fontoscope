de51cbbd0a0884999e16f58858f1ff70082b4e7f
bardoor <dmitrybartenev58@gmail.com>
2025-06-20 16:47:26 +0300

    feat: add FontInfo struct

---
+++ lib/font_info.ex
@@ -0,0 +1,23 @@
+defmodule FontInfo do
+  @moduledoc false
+
+  defstruct [:family, :foundry, :foundry_url, :weight]
+
+  @enforce_keys [:family, :weight]
+
+  @type t :: %__MODULE__{
+    family: String.t(),
+    weight: non_neg_integer(),
+    foundry: String.t() | nil,
+    foundry_url: String.t() | nil
+  }
+
+  @doc """
+  Create a new font info
+  """
+  @spec new(map()) :: t()
+  def new(params) do
+    struct!(__MODULE__, params)
+  end
+
+end
