67102580be023de07f8dea9aade527cf87b6da3e
bardoor <dmitrybartenev58@gmail.com>
2025-06-25 11:55:31 +0300

    feat(fontoscope): add after callback

--- lib/fontoscope.ex
+++ lib/fontoscope.ex
@@ -3,16 +3,23 @@ defmodule Fontoscope do
 
   alias Fontoscope.{AdapterRegistry, FontInfo}
 
+  @type opts :: [{:after, (Path.t() -> any())}]
+
   @doc """
   Extract font info from given file
   """
-  @spec extract(String.t()) :: {:ok, FontInfo.t()} | {:error, String.t()}
-  def extract(file_path) do
+  @spec extract(Path.t(), opts()) :: {:ok, FontInfo.t()} | {:error, String.t()}
+  def extract(file_path, opts \\ []) do
     extension = get_extension(file_path)
 
-    with {:ok, adapter} <- AdapterRegistry.find_by_extension(extension) do
-      adapter.extract(file_path)
-    end
+    result =
+      with {:ok, adapter} <- AdapterRegistry.find_by_extension(extension) do
+        adapter.extract(file_path)
+      end
+
+    run_callbacks(file_path, opts)
+
+    result
   end
 
   defp get_extension(file_path) do
@@ -20,6 +27,13 @@ defmodule Fontoscope do
     |> Path.extname()
     |> String.downcase()
     |> String.trim_leading(".")
+  end
+
+  defp run_callbacks(file_path, opts) do
+    Enum.each(opts, fn
+      {:after, callback} -> callback.(file_path)
+      _ -> nil
+    end)
   end
 
 end
