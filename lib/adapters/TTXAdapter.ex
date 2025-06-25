e085d2db072f8e96f7c1100a6aff29e1478b5270
bardoor <dmitrybartenev58@gmail.com>
2025-06-25 11:29:28 +0300

    feat(ttx-adapter): handle XML parsing errors

--- lib/adapters/TTXAdapter.ex
+++ lib/adapters/TTXAdapter.ex
@@ -6,7 +6,7 @@ defmodule Fontoscope.TTXAdapter do
 
   import SweetXml
 
-  alias Fontoscope.FontInfo
+  alias Fontoscope.{FontInfo, CLI}
 
   @type table_name :: String.t()
 
@@ -15,15 +15,15 @@ defmodule Fontoscope.TTXAdapter do
   """
   @spec extract(String.t()) :: {:ok, FontInfo.t()} | {:error, String.t()}
   def extract(path) do
-    with {:ok, xml_meta} <- tables(path, ~w(name OS/2)) do
+    with {:ok, xml} <- tables(path, ~w(name OS/2)) do
       foundry_id = 9
       foundry_url_id = 12
 
       FontInfo.new(
-        family: family_name(xml_meta),
-        foundry: first_name_id_entry(xml_meta, foundry_id),
-        foundry_url: first_name_id_entry(xml_meta, foundry_url_id),
-        weight: weight(xml_meta)
+        family: family_name(xml),
+        foundry: first_name_id_entry(xml, foundry_id),
+        foundry_url: first_name_id_entry(xml, foundry_url_id),
+        weight: weight(xml)
       )
     end
   end
@@ -34,19 +34,26 @@ defmodule Fontoscope.TTXAdapter do
   For more information about tables see:
   https://learn.microsoft.com/en-us/typography/opentype/spec/otff
   """
-  @spec tables(String.t(), [table_name()]) :: {:ok, String.t()} | {:error, String.t()}
+  @spec tables(String.t(), [table_name()]) :: {:ok, SweetXml.xmlElement()} | {:error, String.t()}
   def tables(path, table_names) do
     table_names = table_names |> Enum.map(&"-t #{&1}") |> Enum.join(" ")
 
     args = ~w(-q -o - #{table_names} #{path})
 
-    case System.cmd("ttx", args, stderr_to_stdout: true) do
-      {content, 0} -> {:ok, to_string(content)}
-      {error, _} -> {:error, error_message(error)}
+    CLI.cmd "ttx", args, [stderr_to_stdout: true],
+      do: &parse_xml/1,
+      else: &make_error/1
+  end
+
+  defp parse_xml(content) do
+    try do
+      {:ok, SweetXml.parse(content)}
+    rescue
+      _ -> {:error, "Failed to parse XML"}
     end
   end
 
-  defp family_name(xml_meta) do
+  defp family_name(xml) do
     # nameID='21' is WWS family name
     # nameID='16' is preferred family name
     # nameID='18' is compatible full macintosh family name
@@ -54,31 +61,31 @@ defmodule Fontoscope.TTXAdapter do
     # nameID='1' is common family name
     # nameID='6' is PostScript name
     Enum.concat([
-      name_id_entries(xml_meta, 21),
-      name_id_entries(xml_meta, 16),
-      name_id_entries(xml_meta, 18),
-      name_id_entries(xml_meta, 4),
-      name_id_entries(xml_meta, 1),
-      name_id_entries(xml_meta, 6)
+      name_id_entries(xml, 21),
+      name_id_entries(xml, 16),
+      name_id_entries(xml, 18),
+      name_id_entries(xml, 4),
+      name_id_entries(xml, 1),
+      name_id_entries(xml, 6)
     ])
     |> List.first()
   end
 
-  defp first_name_id_entry(xml_meta, name_id) do
-    xml_meta
+  defp first_name_id_entry(xml, name_id) do
+    xml
     |> name_id_entries(name_id)
     |> List.first()
   end
 
-  defp weight(xml_meta) do
-    case xpath(xml_meta, ~x"//OS_2/usWeightClass/@value"sl) do
+  defp weight(xml) do
+    case xpath(xml, ~x"//OS_2/usWeightClass/@value"sl) do
       [weight | _] -> String.to_integer(weight)
       _ -> 400
     end
   end
 
-  defp name_id_entries(xml_meta, name_id) do
-    xml_meta
+  defp name_id_entries(xml, name_id) do
+    xml
     |> xpath(~x"//namerecord[@nameID='#{name_id}']/text()"sl)
     |> Enum.map(&String.trim(&1))
     |> Enum.reject(&(&1 == ""))
@@ -86,10 +93,10 @@ defmodule Fontoscope.TTXAdapter do
 
   # We can't disable large help that always printed before the error message
   # So this function extracts actual error message from ttx output
-  defp error_message(output) do
+  defp make_error(output) do
     case Regex.run(~r/ERROR: (.+)$/, output) do
-      nil -> ""
-      [_, error] -> String.trim(error)
+      nil -> {:error, "Unknown TTX error"}
+      [_, error] -> {:error, String.trim(error)}
     end
   end
 
---
+++ lib/adapters/cli.ex
@@ -0,0 +1,11 @@
+defmodule Fontoscope.CLI do
+  @moduledoc false
+
+  def cmd(cmd, args, opts \\ [], do: on_success, else: on_error) do
+    case System.cmd(cmd, args, opts) do
+      {content, 0} -> on_success.(content)
+      {error, _} -> on_error.(error)
+    end
+  end
+
+end
