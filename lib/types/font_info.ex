5d62cffc96a4f41147e29866477fa4a6058e2dcd
bardoor <dmitrybartenev58@gmail.com>
2025-07-07 12:27:33 +0300

    feat(type): use Weight in FontInfo

--- lib/types/font_info.ex
+++ lib/types/font_info.ex
@@ -2,18 +2,17 @@ defmodule Fontoscope.FontInfo do
   @moduledoc false
   use Domo
 
+  alias Fontoscope.Weight
+
   @enforce_keys [:family, :weight, :is_italic]
   defstruct [:family, :foundry, :foundry_url, :weight, :is_italic, :designer]
-
-  @type weight :: non_neg_integer()
-  precond(weight: &(&1 >= 100 and &1 <= 900))
 
   @type nonempty_str :: String.t()
   precond(nonempty_str: &(String.length(String.trim(&1)) > 0))
 
   @type t :: %__MODULE__{
     family: nonempty_str(),
-    weight: weight(),
+    weight: Weight.t(),
     is_italic: boolean(),
     foundry: nonempty_str() | nil,
     foundry_url: nonempty_str() | nil,
