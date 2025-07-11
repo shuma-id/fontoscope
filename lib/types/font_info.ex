8657c0908bae3621022579b703cb9dcb84859f79
bardoor <dmitrybartenev58@gmail.com>
2025-07-11 17:55:16 +0300

    feat(font-info): add class field

--- lib/types/font_info.ex
+++ lib/types/font_info.ex
@@ -2,10 +2,10 @@ defmodule Fontoscope.FontInfo do
   @moduledoc false
   use Domo
 
-  alias Fontoscope.Weight
+  alias Fontoscope.{Weight, FontClass}
 
   @enforce_keys [:family, :weight, :is_italic]
-  defstruct [:family, :foundry, :foundry_url, :weight, :is_italic, :designer]
+  defstruct [:family, :foundry, :foundry_url, :weight, :is_italic, :designer, :class]
 
   @type nonempty_str :: String.t()
   precond(nonempty_str: &(String.length(String.trim(&1)) > 0))
@@ -16,7 +16,8 @@ defmodule Fontoscope.FontInfo do
     is_italic: boolean(),
     foundry: nonempty_str() | nil,
     foundry_url: nonempty_str() | nil,
-    designer: nonempty_str() | nil
+    designer: nonempty_str() | nil,
+    class: FontClass.t() | nil
   }
 
 end
