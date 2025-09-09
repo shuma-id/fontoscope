bfcef8456065350c36b17c1ecef7a2d5d450629c
bardoor <dmitrybartenev58@gmail.com>
2025-09-09 10:05:49 +0300

    feat(core): detect extension by singature only

--- lib/fontoscope.ex
+++ lib/fontoscope.ex
@@ -10,50 +10,40 @@ defmodule Fontoscope do
   """
   @spec extract(Path.t(), opts()) :: {:ok, FontInfo.t()} | {:error, String.t()}
   def extract(file_path, opts \\ []) do
-    extension = detect_extension(file_path)
-
-    result = do_extract(file_path, extension)
-
-    run_callbacks(file_path, opts)
-
-    result
-  end
-
-  defp do_extract(file_path, extension) do
-    with {:ok, adapter} <- AdapterRegistry.find_by_extension(extension) do
-      adapter.extract(file_path)
+    with {:ok, adapter} <- find_adapter(file_path) do
+      result = adapter.extract(file_path)
+      run_callbacks(file_path, opts)
+      result
     end
   end
 
-  defp detect_extension(file_path) do
-    extension = detect_extension_by_path(file_path)
-
-    if extension in AdapterRegistry.supported_extensions() do
-      extension
-    else
-      detect_extension_by_signature(file_path)
+  defp find_adapter(file_path) do
+    with {:ok, extension} <- detect_extension_by_signature(file_path) do
+      AdapterRegistry.find_by_extension(extension)
     end
-  end
-
-  defp detect_extension_by_path(file_path) do
-    file_path
-    |> Path.extname()
-    |> String.downcase()
-    |> String.trim_leading(".")
   end
 
   defp detect_extension_by_signature(file_path) do
-    first_bytes = File.stream!(file_path, 36) |> Enum.at(0)
-
-    case first_bytes do
-      <<0x77, 0x4F, 0x46, 0x46, _rest::binary>> -> "woff"
-      <<0x77, 0x4F, 0x46, 0x32, _rest::binary>> -> "woff2"
-      <<0x00, 0x01, 0x00, 0x00, 0x00, _rest::binary>> -> "ttf"
-      <<0x4F, 0x54, 0x54, 0x4F, _rest::binary>> -> "otf"
-      <<_first::binary-size(34), 0x4C, 0x50, _rest::binary>> -> "eot"
-      _ -> nil
+    with {:ok, file} <- File.open(file_path) do
+      first_bytes = :file.read(file, 36)
+      File.close(file)
+      dispatch_by_signature(first_bytes)
     end
   end
+
+  defp dispatch_by_signature({:ok, first_bytes}) do
+    case first_bytes do
+      <<0x77, 0x4F, 0x46, 0x46, _rest::binary>> -> {:ok, "woff"}
+      <<0x77, 0x4F, 0x46, 0x32, _rest::binary>> -> {:ok, "woff2"}
+      <<0x00, 0x01, 0x00, 0x00, 0x00, _rest::binary>> -> {:ok, "ttf"}
+      <<0x4F, 0x54, 0x54, 0x4F, _rest::binary>> -> {:ok, "otf"}
+      <<_first::binary-size(34), 0x4C, 0x50, _rest::binary>> -> {:ok, "eot"}
+      _ -> {:error, "Unsupported file format"}
+    end
+  end
+
+  defp dispatch_by_signature(:eof), do: {:error, "File may be corrupted or empty"}
+  defp dispatch_by_signature({:error, reason}), do: {:error, "Failed to read file: #{reason}"}
 
   defp run_callbacks(file_path, opts) do
     Enum.each(opts, fn
