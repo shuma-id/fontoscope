4eb67e19427d822f7e8827fc47e9ddad6208c848
bardoor <dmitrybartenev58@gmail.com>
2025-10-15 11:53:43 +0300

    docs(font-class): add moduledoc

--- lib/types/enums.ex
+++ lib/types/enums.ex
@@ -1,6 +1,8 @@
-# According to IBM Font Family Classification
-# https://learn.microsoft.com/en-us/typography/opentype/spec/ibmfc
 defmodule Fontoscope.FontClass do
+  @moduledoc """
+  Font classes accroding to [IBM Font Family Classification](https://learn.microsoft.com/en-us/typography/opentype/spec/ibmfc)
+  """
+
   @type t() :: :unclassified | :oldstyle_serif | :transitional_serif | :modern_serif
              | :clarendon_serif | :slab_serif | :freeform_serif | :sans_serif
              | :ornamental | :script | :symbolic
