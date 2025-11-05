b929fec86a45b8fc9c41c05180e500ed06de89f7
bardoor <dmitrybartenev58@gmail.com>
2025-11-05 10:29:45 +0300

    fix(font-info): reconsider validation

--- lib/types/font_info.ex
+++ lib/types/font_info.ex
@@ -1,6 +1,7 @@
 defmodule Fontoscope.FontInfo do
   @moduledoc false
 
+  require Fontoscope.FontClass
   alias Fontoscope.FontInfo
   alias Fontoscope.{FontClass, Weight}
 
@@ -10,32 +11,42 @@ defmodule Fontoscope.FontInfo do
   @type nonempty_str :: String.t()
 
   @type unique_identifier() :: %{
-    version: String.t(),
-    foundry_tag: String.t(),
-    family: String.t()
-  }
+          version: String.t(),
+          foundry_tag: String.t(),
+          family: String.t()
+        }
 
   @type modifier() :: :italic | :variable
 
   @type t :: %__MODULE__{
-    family: nonempty_str(),
-    weight: Weight.t(),
-    modifiers: [modifier()],
-    foundry: nonempty_str() | nil,
-    foundry_url: nonempty_str() | nil,
-    designer: nonempty_str() | nil,
-    class: FontClass.t() | nil,
-    unique_identifier: unique_identifier() | nil
-  }
+          family: nonempty_str(),
+          weight: Weight.t(),
+          modifiers: [modifier()],
+          foundry: nonempty_str() | nil,
+          foundry_url: nonempty_str() | nil,
+          designer: nonempty_str() | nil,
+          class: FontClass.t() | nil,
+          unique_identifier: unique_identifier() | nil
+        }
 
   @spec new(map() | keyword()) :: {:ok, FontInfo.t()} | {:error, any()}
   def new(params) do
-    params = Map.new(params)
+    params =
+      Map.merge(
+        %{
+          foundry: nil,
+          foundry_url: nil,
+          designer: nil,
+          class: nil,
+          unique_identifier: nil
+        },
+        Map.new(params)
+      )
 
     with {:ok, params} <- validate_family(params),
          {:ok, params} <- validate_weight(params),
          {:ok, params} <- validate_modifiers(params),
-         {:ok, params} <- validate_optional_strings(params),
+         {:ok, params} <- validate_foundry_designer(params),
          {:ok, params} <- validate_class(params),
          {:ok, params} <- validate_unique_identifier(params) do
       {:ok, struct(__MODULE__, params)}
@@ -67,7 +78,7 @@ defmodule Fontoscope.FontInfo do
     do: {:error, %{modifiers: "must be a list"}}
 
   defp validate_modifiers(%{modifiers: modifiers} = params) do
-    valid_modifiers = [:italic, :variable]
+    valid_modifiers = ~w(italic variable)a
 
     if Enum.all?(modifiers, &(&1 in valid_modifiers)) do
       {:ok, params}
@@ -76,52 +87,56 @@ defmodule Fontoscope.FontInfo do
     end
   end
 
-  defp validate_optional_strings(params) do
-    optional_fields = [:foundry, :foundry_url, :designer]
+  defp validate_foundry_designer(params) do
+    optional_fields = ~w(foundry foundry_url designer)a
 
-    Enum.reduce_while(optional_fields, {:ok, params}, fn field, {:ok, acc} ->
-      validate_optional_string_field(acc, field)
+    optional_fields
+    |> Enum.map(&{&1, Map.get(params, &1)})
+    |> Enum.reject(fn {_key, value} -> is_nil(value) end)
+    |> Enum.map(fn {key, value} ->
+      {key, ensure_nonempty_string(value)}
+    end)
+    |> Enum.reduce({:ok, params}, fn
+      {key, {:ok, value}}, {:ok, params} -> {:ok, %{params | key => value}}
+      {key, {:error, reason}}, _acc -> {:error, %{key => reason}}
+      _value, {:error, reason} -> {:error, reason}
     end)
   end
 
-  defp validate_optional_string_field(params, field) do
-    case Map.get(params, field) do
-      nil -> {:cont, {:ok, params}}
-      value when not is_binary(value) -> {:halt, {:error, %{field => "must be a string"}}}
-      value ->
-        trimmed = String.trim(value)
-        if trimmed == "" do
-          {:halt, {:error, %{field => "must be nonempty string"}}}
-        else
-          {:cont, {:ok, %{params | field => trimmed}}}
-        end
+  defp ensure_nonempty_string(value) when not is_binary(value),
+    do: {:error, "must be a string"}
+
+  defp ensure_nonempty_string(value) do
+    trimmed = String.trim(value)
+
+    if trimmed == "" do
+      {:error, "must be nonempty string"}
+    else
+      {:ok, trimmed}
     end
   end
 
   defp validate_class(%{class: nil} = params), do: {:ok, params}
+
   defp validate_class(%{class: class} = params) do
-    valid_classes = [
-      :unclassified, :oldstyle_serif, :transitional_serif, :modern_serif,
-      :clarendon_serif, :slab_serif, :freeform_serif, :sans_serif,
-      :ornamental, :script, :symbolic
-    ]
+    valid_classes = FontClass.values()
 
     if class in valid_classes do
       {:ok, params}
     else
-      {:error, %{class: "must be a valid FontClass"}}
+      {:error, %{class: "must be a valid FontClass, given: #{inspect(class)}"}}
     end
   end
 
   defp validate_unique_identifier(%{unique_identifier: nil} = params), do: {:ok, params}
+
   defp validate_unique_identifier(%{unique_identifier: ui}) when not is_map(ui),
     do: {:error, %{unique_identifier: "must be a map"}}
 
   defp validate_unique_identifier(%{unique_identifier: ui} = params) do
-    required_keys = [:version, :foundry_tag, :family]
+    required_keys = ~w(version foundry_tag family)a
 
-    if Enum.all?(required_keys, &Map.has_key?(ui, &1)) and
-       Enum.all?(required_keys, fn key -> is_binary(Map.get(ui, key)) end) do
+    if Enum.all?(required_keys, fn key -> is_binary(Map.get(ui, key)) end) do
       {:ok, params}
     else
       {:error, %{unique_identifier: "must have version, foundry_tag, and family as strings"}}
