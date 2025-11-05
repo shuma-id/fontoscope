ccc6cfc7a8affaca7a90fe7871075f5cb4437a54
bardoor <dmitrybartenev58@gmail.com>
2025-11-05 10:28:54 +0300

    feat(font-class): add values function

--- lib/types/enums.ex
+++ lib/types/enums.ex
@@ -3,7 +3,23 @@ defmodule Fontoscope.FontClass do
   Font classes accroding to [IBM Font Family Classification](https://learn.microsoft.com/en-us/typography/opentype/spec/ibmfc)
   """
 
-  @type t() :: :unclassified | :oldstyle_serif | :transitional_serif | :modern_serif
-             | :clarendon_serif | :slab_serif | :freeform_serif | :sans_serif
-             | :ornamental | :script | :symbolic
+  @type t() ::
+          :unclassified
+          | :oldstyle_serif
+          | :transitional_serif
+          | :modern_serif
+          | :clarendon_serif
+          | :slab_serif
+          | :freeform_serif
+          | :sans_serif
+          | :ornamental
+          | :script
+          | :symbolic
+
+  @spec values() :: [t, ...]
+  def values do
+    ~w(unclassified oldstyle_serif transitional_serif modern_serif
+      clarendon_serif slab_serif freeform_serif sans_serif
+      ornamental script symbolic)a
+  end
 end
